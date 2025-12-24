c86d46736b81e72cc0e34b4b3dfcee3c3e3d1c5b
Anike247 <j.anike247@gmail.com>
2025-12-24 19:52:01 +0000

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase config (Anike's Cozy Bookshelf)
-export const firebaseConfig = {
-  apiKey: "AIzaSyApfiIqRBOlFNxQECYFayBD8XemLaauNuM",
-  authDomain: "anikes-cozy-bookshelf.firebaseapp.com",
-  projectId: "anikes-cozy-bookshelf",
-  storageBucket: "anikes-cozy-bookshelf.firebasestorage.app",
-  messagingSenderId: "641362242608",
-  appId: "1:641362242608:web:fc62c054de9eeebd4451f5"
-};
